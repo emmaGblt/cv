0cefff984c8b2679e5342332e69d618604ded64e
Emma Guilbault <emma.guilbault@free.fr>
2025-11-11 12:22:36 +0100

    add 2 new skills + add links to the listed resources

--- src/components/AboutMe/AnimatedSkillItem.tsx
+++ src/components/AboutMe/AnimatedSkillItem.tsx
@@ -2,16 +2,17 @@ import type { FC, SVGProps } from "react";
 
 type AnimatedSkillItemProps = {
   Icon: FC<SVGProps<SVGSVGElement>>;
+  href: string;
 };
 
-function AnimatedSkillItem({ Icon }: AnimatedSkillItemProps) {
+function AnimatedSkillItem({ Icon, href }: AnimatedSkillItemProps) {
   // add link to corresponding resource
   // add alt text
   // add hover animation
 
   return (
     <li className="shrink-0 grow-0 flex-1">
-      <a>
+      <a href={href} target="_blank" rel="noopener noreferrer">
         <div className="size-28 holographic-card">
           <Icon className="size-16 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 " />
         </div>
--- src/components/AboutMe/SkillsList.tsx
+++ src/components/AboutMe/SkillsList.tsx
@@ -8,20 +8,36 @@ import TestingLibrary from "@assets/testing-library.svg?react";
 import Python from "@assets/python.svg?react";
 import Javascript from "@assets/javascript.svg?react";
 import Typescript from "@assets/typescript.svg?react";
+import ReactRouter from "@assets/react-router.svg?react";
+import Vite from "@assets/vite.svg?react";
 
 function SkillsList() {
   // gérer le fait que la liste dépasse horizontalement
   return (
     <ul className="absolute top-0 left-0 bottom-0 flex items-center justify-start gap-x-10 px-6">
-      <AnimatedSkillItem Icon={Javascript} />
-      <AnimatedSkillItem Icon={Typescript} />
-      <AnimatedSkillItem Icon={Python} />
-      <AnimatedSkillItem Icon={ReactLogo} />
-      <AnimatedSkillItem Icon={DjangoLogo} />
-      <AnimatedSkillItem Icon={Github} />
-      <AnimatedSkillItem Icon={TailwindCSS} />
-      <AnimatedSkillItem Icon={Vitest} />
-      <AnimatedSkillItem Icon={TestingLibrary} />
+      <AnimatedSkillItem
+        Icon={Javascript}
+        href="https://developer.mozilla.org/fr/docs/Web/JavaScript"
+      />
+      <AnimatedSkillItem
+        Icon={Typescript}
+        href="https://www.typescriptlang.org/"
+      />
+      <AnimatedSkillItem Icon={Python} href="https://www.python.org/" />
+      <AnimatedSkillItem Icon={ReactLogo} href="https://fr.react.dev/" />
+      <AnimatedSkillItem Icon={ReactRouter} href="https://reactrouter.com/" />
+      <AnimatedSkillItem
+        Icon={DjangoLogo}
+        href="https://www.djangoproject.com/"
+      />
+      <AnimatedSkillItem Icon={Github} href="https://github.com/" />
+      <AnimatedSkillItem Icon={TailwindCSS} href="https://tailwindcss.com/" />
+      <AnimatedSkillItem Icon={Vite} href="https://vite.dev/" />
+      <AnimatedSkillItem Icon={Vitest} href="https://vitest.dev/" />
+      <AnimatedSkillItem
+        Icon={TestingLibrary}
+        href="https://testing-library.com/"
+      />
     </ul>
   );
 }
