e7cab2ec101d22097f9240fb2609b15897933a4e
Emma Guilbault <emma.guilbault@free.fr>
2025-11-12 16:52:28 +0100

    add new alias for contexts

--- vite.config.ts
+++ vite.config.ts
@@ -21,6 +21,7 @@ export default defineConfig({
       "@assets": path.resolve(__dirname, "./src/assets"),
       "@components": path.resolve(__dirname, "./src/components"),
       "@utils": path.resolve(__dirname, "./src/utils"),
+      "@contexts": path.resolve(__dirname, "./src/contexts"),
     },
   },
 });
