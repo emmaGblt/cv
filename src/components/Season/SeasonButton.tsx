345514a3a9046e08530e8db945efa6ef574f9054
Emma Guilbault <emma.guilbault@free.fr>
2025-11-12 17:20:57 +0100

    add icon and season name

--- src/components/Season/SeasonButton.tsx
+++ src/components/Season/SeasonButton.tsx
@@ -1,5 +1,7 @@
-import { SeasonContext } from "@/contexts/SeasonContext";
+import { SeasonContext } from "@contexts/SeasonContext";
 import type { TSeason } from "@/types";
+import { cn } from "@utils/classes";
+import { Flower2, Leaf, Snowflake, Sun, type LucideIcon } from "lucide-react";
 import { use } from "react";
 
 type SeasonButtonProps = {
@@ -13,8 +15,23 @@ const ORDERED_SEASONS: Array<TSeason> = [
   "autumn",
 ];
 
+const SEASON_NAMES: Record<TSeason, string> = {
+  winter: "Hiver",
+  spring: "Printemps",
+  summer: "Été",
+  autumn: "Automne",
+};
+
+const SEASON_ICONS: Record<TSeason, LucideIcon> = {
+  winter: Snowflake,
+  spring: Flower2,
+  summer: Sun,
+  autumn: Leaf,
+};
+
 function SeasonButton({ updateSeason }: SeasonButtonProps) {
   const season = use(SeasonContext);
+  const SeasonIcon = SEASON_ICONS[season];
 
   const onSeasonChange = () => {
     const currentSeasonIndex = ORDERED_SEASONS.indexOf(season);
@@ -30,13 +47,19 @@ function SeasonButton({ updateSeason }: SeasonButtonProps) {
     }
   };
 
+  // ADD ANIMATION ON ICON AND TEXT
+
   return (
     <button
       type="button"
       onClick={onSeasonChange}
-      className="absolute top-0 right-0"
+      className={cn(
+        "z-10 cursor-pointer fixed top-6 right-8 min-w-[120px]",
+        "bg-white  border border-gray-400 py-2 px-4 rounded-3xl text-sm flex items-center gap-x-2"
+      )}
     >
-      {season}
+      <SeasonIcon className="size-4 shrink-0" />
+      {SEASON_NAMES[season]}
     </button>
   );
 }
