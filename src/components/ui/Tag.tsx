74f83794bd1d4d0a2aefa41f451d707c66a583e8
Emma Guilbault <emma.guilbault@free.fr>
2025-11-15 22:38:10 +0100

    summer theme

--- src/components/ui/Tag.tsx
+++ src/components/ui/Tag.tsx
@@ -2,7 +2,7 @@ type TypeProps = { text: string };
 
 function Tag({ text }: TypeProps) {
   return (
-    <div className="rounded-xl px-2.5 py-1.5 border-3 inline-block font-semibold text-secondary-2 text-sm shadow bg-secondary-2/30">
+    <div className="rounded-xl px-2.5 py-1.5 border-3 inline-block font-semibold text-secondary-2 text-sm shadow bg-secondary-1/30">
       {text}
     </div>
   );
