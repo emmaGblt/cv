df2f02087f81e67eb153a01a848a7ab69d067fb7
Emma Guilbault <emma.guilbault@free.fr>
2026-06-01 20:40:23 +0200

    improve test again

--- src/components/AboutMe/AboutMeContent.test.tsx
+++ src/components/AboutMe/AboutMeContent.test.tsx
@@ -11,9 +11,16 @@ describe("AboutMeContent renders properly", () => {
 
   afterEach(() => vi.clearAllMocks());
 
-  it("renders 3 headings of level 3", () => {
+  it("renders 4 headings of level 4", () => {
+    expect(screen.getAllByRole("heading", { level: 3 }).length).toBe(4);
     expect(screen.getByRole("heading", { level: 3, name: "Présentation" }));
     expect(screen.getByRole("heading", { level: 3, name: "Projets" }));
+    expect(
+      screen.getByRole("heading", {
+        level: 3,
+        name: "Ce que j'apporte à mon équipe"
+      })
+    );
     expect(screen.getByRole("heading", { level: 3, name: "Technologies" }));
   });
 
--- src/components/AboutMe/AboutMeContent.tsx
+++ src/components/AboutMe/AboutMeContent.tsx
@@ -9,24 +9,28 @@ function AboutMeContent() {
         Présentation
       </h3>
       <div className="flex flex-col gap-y-4 text-justify">
-        <p>
-          Moi c&lsquo;est Emma, développeuse full stack avec{" "}
-          <strong className="font-bold">5 ans d&lsquo;expérience</strong> dans
-          la création d&lsquo;applications web{" "}
-          <strong className="font-bold">
-            scalables et centrées sur leurs utilisateurs
-          </strong>{" "}
-          !
+        <p className="text-lg font-semibold">
+          Moi c&apos;est Emma, développeuse full stack française avec{" "}
+          <strong className="font-bold">5 ans d&apos;expérience</strong> dans la
+          création d&apos;applications web modernes ! J&apos;aime travailler sur
+          des projets <strong className="font-bold">impactants</strong> et{" "}
+          <strong className="font-bold">engagés !</strong>
         </p>
         <p>
-          Je conçois et développe des applications robustes et intuitives, de
-          l&lsquo;interface utilisateur à l&lsquo;architecture backend, en
-          privilégiant la <strong className="font-bold">collaboration</strong>,
-          la <strong className="font-bold">qualité du code</strong>, la{" "}
-          <strong className="font-bold">maintenabilité</strong>, la{" "}
-          <strong className="font-bold">perfomance</strong> et une{" "}
-          <strong className="font-bold">expérience utilisateur optimale</strong>
-          .
+          Je conçois et développe des applications web{" "}
+          <strong className="font-bold">
+            intuitives, maintenables et centrées sur leurs utilisateurs
+          </strong>{" "}
+          en utilisant des technologies modernes et reconnues comme React,
+          Typescript, Python, Django...
+        </p>
+        <p>
+          J&apos;interviens sur l&apos;ensemble du cycle de développement, de
+          l&apos;interface utilisateur au backend, en privilégiant la{" "}
+          <strong className="font-bold">collaboration</strong>, la{" "}
+          <strong className="font-bold">qualité du code</strong>, la{" "}
+          <strong className="font-bold">perfomance</strong> et{" "}
+          <strong className="font-bold">l&apos;expérience utilisateur</strong>.
         </p>
       </div>
       <div className="mt-5 md:mt-8">
@@ -35,13 +39,13 @@ function AboutMeContent() {
         </h3>
         <div className="flex flex-col gap-y-4 text-justify">
           <p>
-            Dans mon parcours, j&lsquo;ai été amenée à :
+            Dans mon parcours, j&apos;ai été amenée à :
             <br />
             &nbsp;-{" "}
             <strong className="font-bold">
               maintenir et développer les nouvelles fonctionnalités
             </strong>{" "}
-            d&lsquo;une application SaaS Djando / React en autonomie
+            d&apos;une application SaaS Djando / React en autonomie
             <br />
             &nbsp;-{" "}
             <strong className="font-bold">
@@ -77,12 +81,31 @@ function AboutMeContent() {
           </p>
         </div>
       </div>
+      <div className="mt-5 md:mt-8">
+        <h3 className="cartoon-title cartoon:text-shadow-md cartoon:text-shadow-primary neo:font-bold mb-2 max-w-fit text-lg md:mb-4 md:text-2xl">
+          Ce que j&apos;apporte à mon équipe
+        </h3>
+        <div className="flex flex-col gap-y-4 text-justify">
+          <p>
+            💪 Mes 5 ans d&apos;expérience en développement full stack
+            <br />
+            💻 Mon expertise en développement backend et frontend
+            <br />
+            🧱 Ma rigueur et mon souci de la qualité du code
+            <br />
+            👥 Mon esprit collaboratif et le partage des mes connaissances
+            <br />
+            ♾️ Ma connaissance des pratiques Devops, de l&apos;infrastructure as
+            code, du déploiement continu, de la conteneurisation...
+          </p>
+        </div>
+      </div>
       <div className="holographic-cards-scrollbar mt-5 md:mt-8">
         <h3 className="cartoon-title cartoon:text-shadow-md cartoon:text-shadow-primary neo:font-bold mb-2 max-w-fit text-lg md:mb-4 md:text-2xl">
           Technologies
         </h3>
         <p>
-          Au fil des années, j&lsquo;ai travaillé avec différentes technologies.
+          Au fil des années, j&apos;ai travaillé avec différentes technologies.
           En voici quelques-unes !
         </p>
 
