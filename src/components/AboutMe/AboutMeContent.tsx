cb3c2b2140585ed79176a3b56d42b6af45fe0965
Emma Guilbault <emma.guilbault@free.fr>
2026-06-01 19:03:36 +0200

    update presentation text

--- src/components/AboutMe/AboutMeContent.tsx
+++ src/components/AboutMe/AboutMeContent.tsx
@@ -5,37 +5,77 @@ import SkillsList from "./SkillsList";
 function AboutMeContent() {
   return (
     <div>
+      <h3 className="cartoon-title cartoon:text-shadow-md cartoon:text-shadow-primary neo:font-bold mb-2 max-w-fit text-lg md:mb-4 md:text-2xl">
+        Présentation
+      </h3>
       <div className="flex flex-col gap-y-4 text-justify">
         <p>
-          Développeuse full stack depuis 5 ans, je travaille sur des{" "}
-          <strong className="font-bold">projets à impact et engagés</strong> !
+          Développeuse full stack avec{" "}
+          <strong className="font-bold">5 ans d&lsquo;expérience</strong> dans
+          la création d&lsquo;applications web{" "}
+          <strong className="font-bold">
+            scalables et centrées sur l&lsquo;utilisateur
+          </strong>
+          .
         </p>
         <p>
-          Dans mon parcours, j&lsquo;ai été amenée à travailler sur des
-          applications Saas avec Django, Ruby on Rails et React (React Router /
-          Remix et Typescript). J&lsquo;ai également participé à la création et
-          à la maintenance d&lsquo;un Design System et d&lsquo;une librairie de
-          composants (Storybook).
+          Je conçois et développe des applications robustes, de
+          l&lsquo;interface utilisateur à l&lsquo;architecture backend, en
+          privilégiant la <strong className="font-bold">collaboration</strong>,
+          la <strong className="font-bold">qualité du code</strong>, la{" "}
+          <strong className="font-bold">maintenabilité</strong>, la{" "}
+          <strong className="font-bold">perfomance</strong> et une{" "}
+          <strong className="font-bold">expérience utilisateur optimale</strong>
+          .
         </p>
-        <p>
-          J&lsquo;aime collaborer, partager mes connaissances et le{" "}
-          <strong className="font-bold">travail d&lsquo;équipe</strong>.
-        </p>
-        <p>
-          J&lsquo;ai à cœur de fournir un code de qualité, robuste et
-          rigoureusement testé.
-        </p>
-        <p>
-          J’ai travaillé au sein d’équipes pluridisciplinaires, en{" "}
-          <strong className="font-bold">collaboration</strong> avec des Product
-          Manager, des Designer et d’autres développeurs, sur des cycles de
-          développement courts et itératifs. J’ai réalisé quotidiennement des{" "}
-          <strong className="font-bold">reviews de code</strong> et participé à
-          des sessions de{" "}
-          <strong className="font-bold">peer programming</strong> et de
-          conception, toujours avec le souci de mettre mon expérience et mes
-          connaissances au service de l’équipe.
-        </p>
+      </div>
+      <div className="mt-5 md:mt-8">
+        <h3 className="cartoon-title cartoon:text-shadow-md cartoon:text-shadow-primary neo:font-bold mb-2 max-w-fit text-lg md:mb-4 md:text-2xl">
+          Projets
+        </h3>
+        <div className="flex flex-col gap-y-4 text-justify">
+          <p>
+            Dans mon parcours, j'ai été amenée à :
+            <br />
+            &nbsp;-{" "}
+            <strong className="font-bold">
+              maintenir et développer les nouvelles fonctionnalités
+            </strong>{" "}
+            d'une application SaaS Djando / React en autonomie
+            <br />
+            &nbsp;-{" "}
+            <strong className="font-bold">
+              concevoir et créer une application
+            </strong>{" "}
+            SaaS Djando / React
+            <br />
+            &nbsp;-{" "}
+            <strong className="font-bold">
+              créer et maintenir un design system
+            </strong>{" "}
+            et une{" "}
+            <strong className="font-bold">librairie de composants</strong>
+            <br />
+            &nbsp;- <strong className="font-bold">
+              migrer des interfaces
+            </strong>{" "}
+            vers des technologies Javascript modernes
+          </p>
+          <p>
+            J’ai travaillé au sein d’équipes pluridisciplinaires, en{" "}
+            <strong className="font-bold">collaboration</strong> avec des
+            Product Manager, des Designer et d’autres développeurs, sur des
+            cycles de développement courts et itératifs.
+          </p>
+          <p>
+            J’ai réalisé quotidiennement des{" "}
+            <strong className="font-bold">reviews de code</strong> et participé
+            à des sessions de{" "}
+            <strong className="font-bold">peer programming</strong> et de
+            conception, toujours avec le souci de mettre mon expérience et mes
+            connaissances au service de l’équipe.
+          </p>
+        </div>
       </div>
       <div className="holographic-cards-scrollbar mt-5 md:mt-8">
         <h3 className="cartoon-title cartoon:text-shadow-md cartoon:text-shadow-primary neo:font-bold mb-2 max-w-fit text-lg md:mb-4 md:text-2xl">
